fa30a08b05ba1a36be17ef575646201cdb0c59a5
Julien Marchand <julien.marchand4@gmail.com>
2018-10-23 15:08:40 +0200

    feat(truffle-provider): add a custom dot env provider

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,22 @@
+require('dotenv').config();
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+const providerWithMnemonic = (mnemonic, rpcEndpoint) =>
+  new HDWalletProvider(mnemonic, rpcEndpoint);
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 7545,
+      network_id: '*', // eslint-disable-line camelcase
+    },
+    dotEnvNetwork: {
+      provider: providerWithMnemonic(
+        process.env.MNEMONIC,
+        process.env.RPC_ENDPOINT
+      ),
+      network_id: parseInt(process.env.NETWORK_ID) || '*'
+    }
+  },
 };
